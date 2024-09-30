b9e33eeb7cfc3be58441e2734e56c3da359810d2
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-30 20:19:24 -0300

    Aula Variaveis 03

---
+++ estrutura_de_programacao/3_convercao/index.js
@@ -0,0 +1,10 @@
+let name3 = 'reste';
+let $nome = 'test2';
+let _nome = 'reste3';
+let firstName ='test';
+
+// let @name = 'teste';
+// let !name = 'teste';
+// let 3name = 'teste';
+
+let Name = 'teste';
