60b090e3cb1b93a58acd2ddbad2098d924908af5
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-30 15:20:52 -0300

    End  Aula 2

---
+++ 2_operacao-aritimetica/index.js
@@ -0,0 +1,7 @@
+console.log(typeof (3 + 2));
+console.log(typeof (5 - 2));
+console.log(typeof (12 * 5));
+console.log(typeof (3 / 2));
+console.log(typeof (20 % 2));
+console.log(typeof (21 % 2));
+
