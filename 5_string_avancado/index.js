783e402f1d03db7a50c801eae102fcca6d0aa26b
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-30 15:44:56 -0300

    End  Aula 5

---
+++ 5_string_avancado/index.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \n Segunda Linha");
+console.log("Meu nome é 'Adson'");
+console.log('O nome dela é "Mari');
+console.log(`A mutiplicacao de 5 por 3 é ${5*3}`);
+console.log('Meu ' + 'amigo '+ 'Calors ' + 'é ' + 'amigo ' + 'da ' + 'Paula.');
