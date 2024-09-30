b4dcf88649174c840ac447a34f7a2e4868bc5ca4
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-30 16:06:07 -0300

    End  Aula 8

---
+++ 8_booleans_operadores_logicos/index.js
@@ -0,0 +1,7 @@
+console.log((5 > 3) && (3 == 2));
+console.log((3 == 2) || ( 5 > 1));
+
+console.log((2 == 2) && ('Adson' == 'Adson'));
+console.log('Adson' == 'Felipe' || false);
+
+console.log(!(true && true));
