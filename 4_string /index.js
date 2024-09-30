47e1c1b155dabfea8a70d6a08f109f97c5f36e8e
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-30 15:32:57 -0300

    End  Aula 4

---
+++ 4_string /index.js
@@ -0,0 +1,5 @@
+console.log(typeof "Meu nome é Adson");
+console.log(typeof 'Eu queria comprar uma casa');
+console.log(typeof `Oi, tudo bem? Como vocè esta?`);
+console.log(typeof 'Infinity');
+console.log( typeof Infinity);
