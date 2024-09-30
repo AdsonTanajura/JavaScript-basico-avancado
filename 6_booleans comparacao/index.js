702ddc328a81284783395d1b7f3af1b1f50c1ea6
AdsonTanajura <adson.tanajura@gmail.com>
2024-09-30 15:52:17 -0300

    End  Aula 7

---
+++ 6_booleans comparacao/index.js
@@ -0,0 +1,10 @@
+console.log(1 > 2);
+console.log(5 < 19);
+console.log(1 >= 1);
+console.log(9 <= 10);
+console.log(5 == 4);
+console.log(4 == '4');
+console.log('Adson' != 'Adson');
+console.log(1 === 1);
+console.log(1 === '1');
+
