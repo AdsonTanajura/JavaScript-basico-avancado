2568787a21c08eeccdb9702e7b9ddd9207d38aab
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-01 15:48:05 -0300

    Aula Variaveis 07

---
+++ estrutura_de_programacao/7_math/index.js
@@ -0,0 +1,15 @@
+const maior = Math.max(6,8,1,5,3,7);
+
+console.log(maior);
+
+const menor = Math.min(6,8,1,5,3,7);
+
+console.log(maior);
+
+const arredondar = Math.round(5.34233323423);
+
+console.log(arredondar);
+
+const arredondarParaCima = Math.ceil(5.34233323423);
+
+console.log(arredondarParaCima);
