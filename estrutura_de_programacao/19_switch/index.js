f69e06f9b258d59ad03a6397e85b8711a3be13c4
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-01 19:14:36 -0300

    Aula Estrutura de Programacao 019

---
+++ estrutura_de_programacao/19_switch/index.js
@@ -0,0 +1,12 @@
+const user = 'Adson';
+
+switch(user) {
+    case "Adson" :
+        console.log('Bem vindo Adson');
+        break;
+    case "Marilia" :
+        console.log("Bem vinda, Marilia");
+        break;
+    default :
+        console.log("Você nao é Adson nem Marilia!!!!");
+};
