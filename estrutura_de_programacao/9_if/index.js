6f8260f6818e3b62ae5b0d36eee0c289693260a5
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-01 15:59:58 -0300

    Aula Variaveis 09

---
+++ estrutura_de_programacao/9_if/index.js
@@ -0,0 +1,9 @@
+const idade = 19;
+const firstName = 'Adson1';
+if(idade <= 18 || firstName != 'Adson') {
+    console.log(`Vejo que você tem ${idade} anos, e se chama ${firstName}, entao desculpa mas você nao pode entrar`);
+
+}
+else if(idade >= 18) {
+    console.log(`Vejo que você tem ${idade} anos, e se chama ${firstName}, entao pode entrar!!`);
+};
