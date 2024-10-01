58397015a642a329d8e6665c019f1a3e56f03dd4
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-01 16:44:29 -0300

    Aula Estrutura de Programacao 012

---
+++ estrutura_de_programacao/12_while/index.js
@@ -0,0 +1,13 @@
+let x = 10;
+
+while(x > 0) {
+    console.log('Atualmente o X esta no valor de ' + x);
+    x--
+};
+
+let y = 0;
+
+while(y <= 10) {
+    console.log('Atualmente o Y esta no valor de ' + y);
+    y++
+};
