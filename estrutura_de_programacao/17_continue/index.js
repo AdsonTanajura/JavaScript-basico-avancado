fb9c64c26eae2f0763d4354cb2c492ef1827d4a1
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-01 17:34:59 -0300

    Aula Estrutura de Programacao 017

---
+++ estrutura_de_programacao/17_continue/index.js
@@ -0,0 +1,7 @@
+console.log('A frente eu tenho um loop que vai contar de 1 a 100, mas so vai mostrar os número que sao divisores de 3');
+
+for(n = 1; n <= 100; n++)  {
+    if(n % 3 == 0) {
+        console.log(n);
+    }
+}
