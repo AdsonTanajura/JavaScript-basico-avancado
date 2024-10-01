67054c2c1a1bf694d3b349cd529b52a46032579a
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-01 17:04:23 -0300

    Aula Estrutura de Programacao 014

---
+++ estrutura_de_programacao/14_for/index.js
@@ -0,0 +1,8 @@
+for(let x = 10; x > 0; x--) {
+    console.log('O valor atual de X é ' + x);
+}
+
+for(let n = 0; n <= 100; n += 2) {
+    console.log('Vou somar de 2 em 2 até 100:      ' + n);
+    
+}
