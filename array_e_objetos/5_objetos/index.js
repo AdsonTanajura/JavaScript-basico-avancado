16bf346cccf977b6e71033f31c020fc0b536ed99
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-02 21:58:36 -0300

    Aula Array e Objetos aula05

---
+++ array_e_objetos/5_objetos/index.js
@@ -0,0 +1,37 @@
+const pessoa = {
+    nome: "João Silva",
+    idade: 30,
+    profissao: "Desenvolvedor",
+    sexo: "Masculino",
+    estadoAtual: "Solteiro",
+    localDeNascimento: "São Paulo",
+    acoes: {
+        fala1: () => {
+            console.log('Boa noite!');
+        },
+        fala2: () => {
+            console.log('Boa tarde!');
+        },
+        fala3: () => {
+            console.log('Boa noite!');
+        }
+    }
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.acoes.fala1());
+
+const carro = {
+    marca: 'Toyoya',
+    ano: 2024,
+    modelo: 'nisan',
+    valor: 2222.22,
+    acoes: {
+        andarFrente: () => {
+            console.log('estou andando para frente');
+        },
+        andarTras: () => {
+            console.log('estou andando para tras');
+        },
+    }
+}
