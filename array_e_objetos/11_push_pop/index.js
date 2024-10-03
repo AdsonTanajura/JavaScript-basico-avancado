3d0f7330f14de255ce1524c888b6e2f75c5f165d
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 08:23:01 -0300

    Aula Array e Objetos aula011

---
+++ array_e_objetos/11_push_pop/index.js
@@ -0,0 +1,11 @@
+const nomes = ["Lucas", "Ana", "João", "Mariana", "Pedro", "Clara", "Gabriel", "Fernanda", "Carlos", "Juliana"];
+console.log(nomes);
+
+const pessoaRemovida = nomes.pop();
+
+console.log(pessoaRemovida);
+console.log(nomes);
+
+nomes.push('Adson');
+
+console.log(nomes);
