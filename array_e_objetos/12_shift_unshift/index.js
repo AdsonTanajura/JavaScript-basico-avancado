283fc87261f94efcbe0b6fc80a3d59e734207594
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 08:29:02 -0300

    Aula Array e Objetos aula012

---
+++ array_e_objetos/12_shift_unshift/index.js
@@ -0,0 +1,13 @@
+const nomes = ["Lucas", "Ana", "João", "Mariana", "Pedro", "Clara", "Gabriel", "Fernanda", "Carlos", "Juliana"];
+console.log(nomes);
+
+const pessoaRemovida = nomes.shift();
+
+console.log(pessoaRemovida);
+console.log(nomes);
+
+nomes.unshift('Adson');
+
+console.log(nomes);
+console.log(nomes[0]);
+
