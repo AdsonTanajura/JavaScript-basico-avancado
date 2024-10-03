d6f365e934e4d2ef28ffed0ca6bcb409f937502f
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 14:13:02 -0300

    Aula Array e Objetos aula017

---
+++ array_e_objetos/17_reverse/index.js
@@ -0,0 +1,5 @@
+const numeros = [15, 32, 48, 64, 79, 85, 93, 105, 117, 123, 137, 142, 155, 168, 174, 189, 196, 205, 214, 223, 234, 245, 256, 269, 278, 289, 302, 315, 328, 334, 349, 359, 372, 386, 399, 412, 426, 435, 448, 459];
+console.log(numeros);
+const invertNumeros = numeros.reverse();
+
+console.log(invertNumeros);
