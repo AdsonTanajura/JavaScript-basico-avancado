8463ee35e886cb0fea76ba631a67d4c924ec5515
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 16:16:54 -0300

    Ex 034

---
+++ Exercicios/Ex034/index.js
@@ -0,0 +1,7 @@
+const vida = 'Cada pequeno passo dado hoje nos aproxima dos grandes sonhos de amanhã';
+
+const splitVida = vida.split(' ');
+
+for(let n = 0; n < splitVida.length; n++) {
+  console.log(splitVida[n]);
+}
