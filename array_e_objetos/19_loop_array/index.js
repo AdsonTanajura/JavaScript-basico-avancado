dee6c97c010799560160172068ffaed3d932a540
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 08:18:53 -0300

    Aula Array e Objetos aula010

---
+++ array_e_objetos/19_loop_array/index.js
@@ -0,0 +1,5 @@
+const nomes = ["Lucas", "Ana", "João", "Mariana", "Pedro", "Clara", "Gabriel", "Fernanda", "Carlos", "Juliana"];
+
+for(let n = 0; n < nomes.length; n++) {
+    console.log(nomes[n]);
+};
