88eac2850c84da25ad6f747664780711336477dd
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 14:52:22 -0300

    Aula Array e Objetos aula21

---
+++ array_e_objetos/21_join/index.js
@@ -0,0 +1,9 @@
+const fullName = 'Adson Nunes Tanajura';
+console.log('Seu nome é ' + fullName);
+console.log('Eu vou inverter seu nome!!!')
+
+const splitName = fullName.split(' ');
+
+const nomeInvertido = splitName.reverse().join(' ');
+
+console.log(nomeInvertido);
