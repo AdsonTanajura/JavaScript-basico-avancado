e4bc70595e5b0df25778fba37dc5e3ea3e8b0f9e
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 15:35:13 -0300

    Ex 028

---
+++ Exercicios/Ex028/index.js
@@ -0,0 +1,11 @@
+const onibus = {
+    rodas: 8,
+    limitePassageros: 40,
+    portas: 2,
+};
+
+const { limitePassageros, portas, rodas } = onibus;
+
+console.log(limitePassageros);
+console.log(portas);
+console.log(rodas);
