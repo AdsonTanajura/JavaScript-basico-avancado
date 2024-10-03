ee88558cf05bb6189ae40cec813d324915d18b2d
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 09:19:35 -0300

    Aula Array e Objetos aula014

---
+++ array_e_objetos/15_forEach/index.js
@@ -0,0 +1,7 @@
+const numeros = [5, 12, -3, 8, 19, 7, -15, 22, 0, 4];
+let total = 0;
+numeros.forEach(num => {
+    total += num;
+})
+console.log('Eu vou somar todos esses numeros: ' + numeros );
+console.log('O total dar: ' + total);
