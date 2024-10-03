d0a9097b7c4278c0db9cb4ca387aa5acba4dffc8
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-02 21:38:05 -0300

    Aula Array e Objetos aula02

---
+++ array_e_objetos/2_mais_sobre_arrays/index.js
@@ -0,0 +1,7 @@
+const numeros = [3, -5, 7, -2, 10, -1, 0, 8, -6, 4];
+const nomes = ["Lucas", "Ana", "João", "Mariana", "Pedro", "Clara", "Gabriel", "Fernanda", "Carlos", "Juliana"];
+const booleanos = [true, false, true, false, true, false, true, false, true, false];
+
+console.log(numeros[4]);
+console.log(nomes[5]);
+console.log(booleanos[1]);
