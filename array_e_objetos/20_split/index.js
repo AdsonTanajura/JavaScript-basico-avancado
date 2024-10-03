b77efbb4b60146f76e8143ef3ad2388c2af1a874
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 14:48:56 -0300

    Aula Array e Objetos aula20

---
+++ array_e_objetos/20_split/index.js
@@ -0,0 +1,6 @@
+const fullName = 'Adson Nunes Tanajura';
+
+const splitName = fullName.split(' ');
+
+console.log(`Seu primeiro nome é: ${splitName[0]}`);
+console.log(`Seu Sobre Nome é: ${splitName[1]} ${splitName[2]}`);
