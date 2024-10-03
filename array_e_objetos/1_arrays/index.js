b6f7aaa66a5c65c4a0904887f94d5c3054cc38ab
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-02 21:34:57 -0300

    Aula Array e Objetos aula01

---
+++ array_e_objetos/1_arrays/index.js
@@ -0,0 +1,9 @@
+const numeros = [3, -5, 7, -2, 10, -1, 0, 8, -6, 4];
+const nomes = ["Lucas", "Ana", "João", "Mariana", "Pedro", "Clara", "Gabriel", "Fernanda", "Carlos", "Juliana"];
+const booleanos = [true, false, true, false, true, false, true, false, true, false];
+const misturado = [42, "JavaScript", true, -7, "Frontend", false, { nome: "Lucas", idade: 25 }, null, 3.14, "Hello"];
+
+console.log(numeros);
+console.log(nomes);
+console.log(booleanos);
+console.log(misturado);
