18b106992a093aae6dc8aa0117d2db22f6653a4d
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 08:35:18 -0300

    Aula Array e Objetos aula013

---
+++ array_e_objetos/13_indexOf_lastIndexOf/index.js
@@ -0,0 +1,9 @@
+const numeros = [5, 12, 4, 8, 19, 7, 4, 22, 0, 4];
+
+console.log(numeros.indexOf(4));
+console.log(numeros.lastIndexOf(4));
+
+
+const nomes = ["Lucas", "Ana", "João", "Mariana", "Pedro", "Clara", "Gabriel", "Fernanda", "Carlos", "Juliana"];
+
+console.log(nomes.indexOf('Ana'));
