099f333a4b04012096490b89c1a12514a3e66295
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 15:08:24 -0300

    Aula Array e Objetos aula25

---
+++ array_e_objetos/24_destructuring_array/index.js
@@ -0,0 +1,7 @@
+const nomes = ["Lucas", "Ana", "João", "Mariana", "Pedro", "Clara", "Gabriel", "Fernanda", "Carlos", "Juliana"];
+
+const [nome1, nome2, nome3] = nomes;
+
+console.log(nome1);
+console.log(nome2);
+console.log(nome3);
