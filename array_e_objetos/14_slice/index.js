72e5047103c7279a47b97f2ac922cc91af6e605b
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 08:56:14 -0300

    Aula Array e Objetos aula014

---
+++ array_e_objetos/14_slice/index.js
@@ -0,0 +1,9 @@
+const numeros = [5, 12, 4, 8, 19, 7, 4, 22, 0, 4];
+
+const sliceArray = (numeros.slice(2,7));
+
+console.log(sliceArray);
+
+const nomes = ["Lucas", "Ana", "João", "Mariana", "Pedro", "Clara", "Gabriel", "Fernanda", "Carlos", "Juliana"];
+
+console.log(nomes.slice(1,3));
