0c2b45e52e8b6ace8a0c7a4979e494491c1d303d
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-02 21:42:33 -0300

    Aula Array e Objetos aula03

---
+++ array_e_objetos/3_propiedades/index.js
@@ -0,0 +1,12 @@
+const nome = "Adson";
+
+console.log(nome.length);
+
+const numeros = [10, -5, 23, 42, 0, 17, -8, 99, 64, 3];
+
+console.log(numeros.length);
+
+console.log(numeros['length']);
+
+console.log(numeros[2]);
+
