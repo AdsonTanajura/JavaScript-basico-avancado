d7549499117b4cfa9bee2a7fc66e922d4024a12f
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-02 22:12:02 -0300

    Aula Array e Objetos aula08

---
+++ array_e_objetos/8_chaves_de_um_objeto/index.js
@@ -0,0 +1,21 @@
+const pessoa = {
+    nome: "João Silva",
+    idade: 30,
+    profissao: "Desenvolvedor",
+    sexo: "Masculino",
+    estadoAtual: "Solteiro",
+    localDeNascimento: "São Paulo",
+    acoes: {
+        fala1: () => {
+            console.log('Boa noite!');
+        },
+        fala2: () => {
+            console.log('Boa tarde!');
+        },
+        fala3: () => {
+            console.log('Boa noite!');
+        }
+    }
+};
+
+console.log(Object.keys(pessoa));
