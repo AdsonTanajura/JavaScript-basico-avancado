f54c468ad92c17b69f3cb25f4d38c7fb9161962d
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-04 16:06:17 -0300

    regex 06

---
+++ Regex/6_operador_plus+/index.js
@@ -0,0 +1,5 @@
+const muitosOuPoucosDigitos = /\d+/;
+
+console.log(muitosOuPoucosDigitos.test("123"));
+console.log(muitosOuPoucosDigitos.test("123456789"));
+console.log(muitosOuPoucosDigitos.test(""));
