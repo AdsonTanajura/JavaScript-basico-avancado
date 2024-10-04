b1ae3f81109a339e56c2052b0f97ffa41999f4af
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 23:46:28 -0300

    POO Aula15

---
+++ POO/15_instanceof/index.js
@@ -0,0 +1,28 @@
+class Veiculo {
+  constructor({name, type, motor}) {
+    this.name = name;
+    this.type = type;
+    this.motor = motor;
+  };
+  andar() {
+    console.log('Estou andando');
+  }
+}
+class Car extends Veiculo {
+  constructor({motor, masxSpeed, porta, name, type}) {
+  super({name, motor, type});
+  this.masxSpeed = masxSpeed;
+  this.porta = porta;
+  this.roda = 4;
+  };
+}
+
+const bmw = new Car({name: 'BMW', type: 'car', motor: true, masxSpeed: 120, porta: 2})
+
+console.log(bmw.name);
+console.log(bmw.motor);
+bmw.andar();
+
+
+console.log(bmw instanceof Veiculo);
+
