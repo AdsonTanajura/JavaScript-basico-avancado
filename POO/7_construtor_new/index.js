e2a40f5cfab5c5cd46cac97a9887cc7eadd62121
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 22:13:21 -0300

    POO Aula07

--- POO/7_construtor_new/index.js
+++ POO/7_construtor_new/index.js
@@ -1,14 +1,12 @@
-
-function criarCachorro({raca}){
-    const dog = Object.create({});
-    dog.raca = raca;
-
-    return dog;
+function Pessoa({nome, idade, sexo}) {
+    this.nome = nome;
+    this.idade = idade;
+    this.sexo = sexo;
+    this.andar = function() {
+      return 'Estou andando!!';
+    }
 }
 
+const adson = new Pessoa({nome: 'Adson', idade: 22, sexo: 'Masculino'});
 
-
-const pastorAlemao = criarCachorro('Pastor Alemao');
-
-
-console.log(pastorAlemao.raca);
+console.log(adson.andar());
