c08fdbf057ea6cfa2e41038409a9e7a2eb49b642
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-03 22:17:28 -0300

    POO Aula08

---
+++ POO/8_metodo_prototype/index.js
@@ -0,0 +1,13 @@
+function Pessoa({nome, idade, sexo}) {
+    this.nome = nome;
+    this.idade = idade;
+    this.sexo = sexo;
+}
+
+Pessoa.prototype.andar = () => {
+  return 'Estou andando!!'
+}
+
+const adson = new Pessoa({nome: 'Adson', idade: 22, sexo: 'Masculino'});
+
+console.log(adson.andar());
