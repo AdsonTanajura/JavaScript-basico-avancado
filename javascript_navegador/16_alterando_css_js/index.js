0b99bb6501bdb4bfac4d62fccd3f72257d6fdaba
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-05 19:55:14 -0300

    JavaScript Navegador 16

---
+++ javascript_navegador/16_alterando_css_js/index.js
@@ -0,0 +1,13 @@
+const elemento = document.querySelector('#first-h1');
+const firstDiv = document.querySelector('#container-principal');
+firstDiv.style.backgroundColor = 'red';
+firstDiv.style.display = 'flex';
+firstDiv.style.flexDirection = 'column';
+firstDiv.style.alignItems = 'center';
+
+elemento.style.color = 'blue';
+
+
+
+
+
