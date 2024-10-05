259b0a8f9818edb7a8ea034fe247e19466f5cdc4
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-05 20:53:22 -0300

    JavaScript Eventos 05

---
+++ evento_javascript/5_prevent_defult/index.js
@@ -0,0 +1,7 @@
+const fakeA = document.querySelector('#fake-a');
+
+fakeA.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('Ta aqui o link, entra você mesmo!!!! :3');
+    console.log('https://www.youtube.com/');
+})
