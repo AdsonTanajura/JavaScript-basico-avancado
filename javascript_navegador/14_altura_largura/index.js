60d90626e2fa950f7c591d2aa504529b2e79d955
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-05 19:41:41 -0300

    JavaScript Navegador 14

---
+++ javascript_navegador/14_altura_largura/index.js
@@ -0,0 +1,7 @@
+const elemento = document.querySelector('#first-h1');
+
+console.log(elemento.offsetWidth);
+console.log(elemento.offsetHeight);
+
+console.log(elemento.clientHeight);
+console.log(elemento.clientWidth);
