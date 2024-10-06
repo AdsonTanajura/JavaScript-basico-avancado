af73cbd734a0bfca3367be9bf8feba8f23a4b807
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-05 21:36:30 -0300

    JavaScript Eventos 07

--- evento_javascript/7_mais_eventos-mouse/index.js
+++ evento_javascript/7_mais_eventos-mouse/index.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('button');
+const button = document.querySelector('#haha');
 
 button.addEventListener('click', () => {
     window.location.href = 'https://www.youtube.com/';
@@ -7,3 +7,13 @@ button.addEventListener('click', () => {
 button.addEventListener('dblclick', () => {
     window.location.href = 'https://www.youtube.com/watch?v=ZUHh2PU3Lao&list=RDZUHh2PU3Lao&start_radio=1';
 })
+
+const button2 = document.querySelector('#hihi');
+
+button2.addEventListener('mousup', () => {
+    console.log('Soltou o botao');
+});
+
+button2.addEventListener('mousedown', () => {
+    console.log('Apretou o botao');
+});
