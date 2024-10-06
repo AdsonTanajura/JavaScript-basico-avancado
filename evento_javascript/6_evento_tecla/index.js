a4370f44f0aab45b32c12a7d1dd8f3c4877439bf
AdsonTanajura <adson.tanajura@gmail.com>
2024-10-05 21:00:50 -0300

    JavaScript Eventos 06

---
+++ evento_javascript/6_evento_tecla/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('keydown', (e) => {
+    if(e.key == 'a') {
+        console.log('Você acetou a leta!! Era a letra A');
+    } else {
+        console.log('Nao é a telca ' + e.key + ' :c' );
+    }
+})
